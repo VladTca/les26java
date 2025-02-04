79c460f0356655f6fff2e23afa3d76405d876459
VladTca <votsats@gmail.com>
2025-02-04 22:26:24 +0100

    ♻️ Рефакторинг кода.

--- src/Person.java
+++ src/Person.java
@@ -13,28 +13,17 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
         return getAge() == person.getAge() && Objects.equals(getName(), person.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getAge());
     }
 
     @Override
